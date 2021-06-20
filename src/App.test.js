cae407f98cdcbb7589d96170cd23e414434400dc
Soundhar <laptopisasset@gmail.com>
2021-06-20 12:16:38 +0530

    ColorChange Button completed.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("button has correct initail color", () => {
   render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+
+  // find an element with  role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole("button", { name: /change to blue/i });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: "red" });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect to have a background color blue
+  expect(colorButton).toHaveStyle({ backgroundColor: "blue" });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe("Change to red");
 });
